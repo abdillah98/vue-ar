8bafba2a983469c1336f6e054f09153a55f7d96a
Abdillah AG <abdillahag98@gmail.com>
2024-07-29 09:37:31 +0700

    modify utils/helpers.js

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,6 @@
 export function getRootUrl() {
   const { protocol, hostname, port } = window.location;
   const baseURL = `${protocol}//${hostname}${port ? `:${port}` : ''}`;
-  return process.env.NODE_ENV === 'production' ? baseURL + '/vue-ar' : baseURL
+  // return process.env.NODE_ENV === 'production' ? baseURL + '/vue-ar' : baseURL
+  return baseURL
 }
